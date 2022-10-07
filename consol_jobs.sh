fdb7caf103761dac3c8489ad3b8d0d0870858b1c
Tam Zher Min Zac <zac.tamzm@shopee.com>
2022-10-07 22:19:56 +0800

    Edit consol_jobs.sh

--- consol_jobs.sh
+++ consol_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-latestjobid=$(squeue --nohead --format %F | tail -n 1)
-cmd1="sbatch --dependency=afterok:${latestjobid}"
-cmd1="${cmd1} /data/src/PyHipp/ec2snapshot.sh"
+jobIds=$(squeue --nohead --format %F)
+jobIds=(${jobIds//$'\n'/:})
+cmd1="sbatch --dependency=afterok:${jobIds} /data/src/PyHipp/ec2snapshot.sh"
 
 echo $cmd1
 eval $cmd1
