395fdbb4dad6d66878d98d5e37b91dd4e44171d8
AWS ParallelCluster user <ec2-user@ip-10-0-7-6.ap-southeast-1.compute.internal>
2022-10-28 09:58:48 +0000

    Add Lab 7 freq scripts

---
+++ fsall-slurm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --cpus-per-task=1   # number of CPUs for this task
+#SBATCH -J "freq"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o freq-slurm.%N.%j.out # STDOUT
+#SBATCH -e freq-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+import DataProcessingTools as DPT; \
+lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
+lfall.save(); \
+hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
+hfall.save();
+
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:298402190365:awsnotify --message "FSJobDone"
